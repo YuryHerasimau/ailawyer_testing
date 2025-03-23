eef49c68187b3f31d2891e9facd4a9be9d639d76
Yury Herasimau <yl.herasimau@gmail.com>
2025-03-23 21:41:58 +0300

    Add: codegen test for login

---
+++ main.py
@@ -0,0 +1,58 @@
+import os
+import time
+from playwright.sync_api import Playwright, sync_playwright, expect
+from dotenv import load_dotenv
+
+load_dotenv()
+
+GOOGLE_EMAIL = os.getenv("GOOGLE_EMAIL")
+GOOGLE_PASS = os.getenv("GOOGLE_PASS")
+
+
+def run(playwright: Playwright) -> None:
+    # запуск браузера chromium
+    browser = playwright.chromium.launch(
+        headless=False,
+        args=["--disable-blink-features=AutomationControlled"]
+    )
+    # создает изолированный сеанс браузера
+    context = browser.new_context(viewport={"width": 1700, "height": 900})
+    # открывает новую страницу(tab) в браузере
+    page = context.new_page()
+
+    page.goto("https://app.ailawyer.pro/")
+    page.get_by_role("radio", name="I'm a legal consumer").click()
+    page.get_by_role("button", name="Choose and continue").click()
+
+    with page.expect_popup() as page1_info:
+        page.get_by_role("button").first.click()
+
+    page1 = page1_info.value
+
+    page1.get_by_role("textbox", name="Email or phone").fill(GOOGLE_EMAIL)
+    page1.get_by_role("textbox", name="Email or phone").click()
+    page1.get_by_role("button", name="Next").click()
+
+    page1.get_by_role("textbox", name="Enter your password").fill(GOOGLE_PASS)
+    page1.get_by_role("textbox", name="Enter your password").click()
+    page1.get_by_role("button", name="Next").click()
+
+    # Ожидание перехода на страницу чатов
+    expect(page).to_have_url("https://app.ailawyer.pro/chats/", timeout=30000)
+
+    # Проверка наличия надписи "New chat" в хедере
+    expect(page.get_by_role("heading", name="New Chat")).to_be_visible()
+
+    # Проверка наличия кнопки с надписью "New chat" в сайдбаре
+    expect(page.get_by_role("button", name="New Chat")).to_be_visible()
+
+    time.sleep(10)
+
+    context.close()
+    browser.close()
+
+    print("Тест прошел успешно")
+
+
+with sync_playwright() as playwright:
+    run(playwright)
